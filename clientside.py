6e15781106de7517c0b8002cd536da1db0688b0c
Andrew (Minty) <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-17 03:06:07 +0400

    dumbest mistake of my life

--- clientside.py
+++ clientside.py
@@ -52,7 +52,7 @@ while True:
         break
     # add the datetime, name & the color of the sender
     date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-    to_send = f"{client_color}[{date_nw}] {name}{separator_token}{to_send}{Fore.RESET}"
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
     # finally, send the message
     s.send(to_send.encode())
 
